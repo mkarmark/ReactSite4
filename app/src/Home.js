1e468667d7ad0fa11535d9dd70afaa6676c774b5
Mitali Karmarkar <mikarmar@microsoft.com>
2020-10-23 10:51:37 -0700

    init commit

---
+++ app/src/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to my test site! This site was last modified on 9/16/20 8:58am
+        </p>
+        <a
+          className="App-link"
+          href="/shoppingList/"
+        >
+          Let's try some functions!
+        </a>
+        <a 
+          className="App-link"
+          href="/about/"
+        >
+        Gated content
+        </a>
+		        <a 
+          className="App-link"
+          href="/.auth/login/facebook"
+        >
+        Log in with Facebook
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default Home;
